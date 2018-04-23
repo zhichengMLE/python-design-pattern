316ae696615680069c56c4f264423ae649b387ea
zhichengMLE <jason-dean@outlook.com>
2018-04-22 20:58:03 -0400

    Fix error in improvement 1

--- Structural Patterns/imporvement_1.py
+++ Structural Patterns/imporvement_1.py
@@ -15,47 +15,32 @@ def f4():
     print('f4')
 
 # Project 1
-def proj1_validation_f1():
-    print('project 1 validation f1')
+def proj1_validation():
+    print('project 1 validation')
 
-def proj1_validation_f2():
-    print('project 1 validation f2')
-
-def proj1_validation_f4():
-    print('project 1 validation f4')
-
-proj1_validation_f1()
+proj1_validation()
 f1()
-proj1_validation_f2()
+proj1_validation()
 f2()
-proj1_validation_f4()
+proj1_validation()
 f4()
 
 # Project 2
-def proj2_validation_f2():
-    print('project 2 validation f2')
+def proj2_validation():
+    print('project 2 validation')
 
-def proj2_validation_f3():
-    print('project 2 validation f3')
-
-def proj2_validation_f4():
-    print('project 2 validation f4')
-
-proj2_validation_f2()
+proj2_validation()
 f2()
-proj2_validation_f3()
+proj2_validation()
 f3()
-proj2_validation_f4()
+proj2_validation()
 f4()
 
 # Project 3
-def proj3_validation_f2():
-    print('project 3 validation f2')
+def proj3_validation():
+    print('project 3 validation')
 
-def proj3_validation_f3():
-    print('project 3 validation f3')
-
-proj3_validation_f2()
+proj3_validation()
 f2()
-proj3_validation_f3()
+proj3_validation()
 f3()
