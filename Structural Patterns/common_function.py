79103fbb204a934ddc6e141ecd2f148a90c347dc
zhichengMLE <jason-dean@outlook.com>
2018-04-22 20:51:45 -0400

    Add common function

---
+++ Structural Patterns/common_function.py
@@ -0,0 +1,27 @@
+# These are the common_function which are being used in many different projects.
+# But now, you are assigned to adding validation function ahead of running each function.
+def f1():
+    print('f1')
+
+def f2():
+    print('f2')
+
+def f3():
+    print('f3')
+
+def f4():
+    print('f4')
+
+# Project 1
+f1()
+f2()
+f4()
+
+# Project 2
+f2()
+f3()
+f4()
+
+# Project 3
+f1()
+f3()
